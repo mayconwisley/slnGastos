7f1f0704101c4801acee74e0df8dccf428b32a53
Maycon Wisley <maycon.wisley@outlook.com>
2026-08-22 00:36:25 -0300

    Enable Enter key to submit login from PasswordBox
    PasswordBox in LoginWindow now handles KeyDown to detect Enter.
    Added SenhaLoginTeclaPressionada handler in code-behind, triggering EntrarCommand on Enter key press for improved usability.

--- Gastos/Presentation/Views/LoginWindow.xaml.cs
+++ Gastos/Presentation/Views/LoginWindow.xaml.cs
@@ -1,5 +1,6 @@
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Input;
 using Gastos.Presentation.ViewModels;
 
 namespace Gastos.Presentation.Views;
@@ -22,4 +23,15 @@ public partial class LoginWindow : Window
     }
 
     private void SenhaLoginAlterada(object sender, RoutedEventArgs e) => viewModel.Senha = ((PasswordBox)sender).Password;
+
+    private void SenhaLoginTeclaPressionada(object sender, KeyEventArgs e)
+    {
+        if (e.Key != Key.Enter || !viewModel.EntrarCommand.CanExecute(null))
+        {
+            return;
+        }
+
+        viewModel.EntrarCommand.Execute(null);
+        e.Handled = true;
+    }
 }
